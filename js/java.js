a2e6aae0fea116cfa4c4039b9e5d38d1b85a659f
Samuel william <swzubek@hotmail.com>
2021-09-02 20:02:10 -0300

    Primeiro commit

---
+++ js/java.js
@@ -0,0 +1,7 @@
+function informacao(){
+    window.open("https://www.adorocinema.com/filmes/filme-58525/curiosidades/")
+}
+
+function assistir(){
+    window.open("https://www.youtube.com/watch?v=36mnx8dBbGE");
+}
---
+++ js/owl/setup.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function botao(){
+    window.open("https://www.youtube.com/watch?v=36mnx8dBbGE");
+}
+
